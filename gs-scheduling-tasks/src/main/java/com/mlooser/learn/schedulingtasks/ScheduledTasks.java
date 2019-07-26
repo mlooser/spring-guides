cd92f17d1428668b26190019755f3983f23a589e
mlooser <mlooser@gmail.com>
2019-07-26 09:36:17 +0200

    [gs-scheduling-tasks] Done project.

---
+++ gs-scheduling-tasks/src/main/java/com/mlooser/learn/schedulingtasks/ScheduledTasks.java
@@ -0,0 +1,20 @@
+package com.mlooser.learn.schedulingtasks;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+  private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+  private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss");
+  
+  @Scheduled(fixedRate = 5000)
+  public void reportTime() {
+    log.info("The time is now {}", DATE_FORMAT.format(new Date()));
+  }
+}
