41719da4050e50170e1c0698b6da1e3a6eee14d8
mlooser <mlooser@gmail.com>
2020-02-06 10:52:59 +0100

    [gs-batch-processing] Done project.

---
+++ gs-batch-processing/src/test/java/com/mlooser/learn/batchprocessing/BatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlooser.learn.batchprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
